6d0155bf0e8da4244726c79f7843a6a172627d8c
Oliver Coughlan <39098447+OliverCoughlan@users.noreply.github.com>
2023-08-08 15:30:28 +0100

    Update UNetTrain.py

--- UNetTrain.py
+++ UNetTrain.py
@@ -24,18 +24,15 @@ import config
 dev = torch.device("cuda:3")
 
 
-#split up training and testing imgs
 noPatients = 0
 subDirList = []
 cd = "/store/DAMTP/ab2860/AItomotools/data/AItomotools/processed/LIDC-IDRI/"
 for subdir in os.listdir(cd):
 	f = os.path.join(cd, subdir)
-	# checking if it is a file
 	if not os.path.isfile(f):
 		noPatients += 1
 		subDirList.append(f)
 
-#random.shuffle(subDirList)
 
 trainNo = int(np.round(noPatients * 0.8))
 validateNo = int(np.round(noPatients * 0.1))
@@ -48,14 +45,12 @@ testList = subDirList[trainNo+validateNo:]
 
 
 
-# create the train and test datasets
 trainDS = loadData(imgPaths=trainList, outputSino=False)
 testDS = loadData(imgPaths=testList, outputSino=False)
 print(f"[INFO] found {len(trainDS)} examples in the training set...")
 print(f"[INFO] found {len(testDS)} examples in the test set...")
 
 
-# create the training and test data loaders
 trainLoader = DataLoader(trainDS, shuffle=True,
 	batch_size=config.BATCH_SIZE, pin_memory=False)
 
@@ -64,66 +59,44 @@ testLoader = DataLoader(testDS, shuffle=False,
 	batch_size=config.BATCH_SIZE, pin_memory=False)
 
 
-# initialize our UNet model
 unet = UNet().to(dev)
-# initialize loss function and optimizer
 lossFunc = nn.MSELoss()
 opt = Adam(unet.parameters(), lr=config.INIT_LR)
-# calculate steps per epoch for training and test set
 trainSteps = len(trainDS) // config.BATCH_SIZE
 testSteps = len(testDS) // config.BATCH_SIZE
-# initialize a dictionary to store training history
 H = {"train_loss": [], "test_loss": []}
 
 
-# loop over epochs
 startTime = time.time()
 print("Training started at: ", startTime)
 
 valMin = 100
 
-#tqdm gives a progress bar showing how much training done
 for e in tqdm(range(config.NUM_EPOCHS)):
-	# set the model in training mode
 	unet.train()
-	# initialize the total traininçg and validation loss
 	totalTrainLoss = 0
 	totalTestLoss = 0
-	# loop over the training set
 	for (i, (x, y)) in enumerate(trainLoader):
-		# send the input to the device
 		(x, y) = (x.to(dev), y.to(dev))
-		# perform a forward pass and calculate the training loss
 		pred = unet(x)
-		#pred = pred / torch.max(pred)
-		#y = y / torch.max(y)
+
 		loss = lossFunc(pred, y)
-		# first, zero out any previously accumulated gradients, then
-		# perform backpropagation, and then update model parameters
+
 		opt.zero_grad()
 		loss.backward()
 		opt.step()
-		# add the loss to the total training loss so far
 		totalTrainLoss += loss
-	# switch off autograd
 	with torch.no_grad():
-		# set the model in evaluation modde
 		unet.eval()
-		# loop over the validation set
 		for (x, y) in testLoader:
-			# send the input to the device
 			(x, y) = (x.to(dev), y.to(dev))
-			# make the predictions and calculate the validation loss
 			pred = unet(x)
 			totalTestLoss += lossFunc(pred, y)
-	# calculate the average training and validation loss
 	avgTrainLoss = totalTrainLoss / trainSteps
 	avgTestLoss = totalTestLoss / testSteps
-	# update our training history
 	H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
 	H["test_loss"].append(avgTestLoss.cpu().detach().numpy())
 	valLoss = avgTestLoss.cpu().detach().numpy()
-	# print the model training and validation information
 	print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
 	print("Train loss: {:.6f}, Test loss: {:.4f}".format(
 		avgTrainLoss, avgTestLoss))
@@ -140,7 +113,6 @@ for e in tqdm(range(config.NUM_EPOCHS)):
 		valMin = valLoss
 		torch.save(unet.state_dict(),  "/local/scratch/public/obc22/UNetTrainCheckpts/UNetV2MinValSD.pth")
 
-# display the total time needed to perform the training
 endTime = time.time()
 print("[INFO] total time taken to train the model: {:.2f}s".format(
 	endTime - startTime))
